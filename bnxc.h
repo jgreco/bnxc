bd7d0c91930232e45f45b91d148d02e5d10607ee
John Greco <greco@drexel.edu>
2009-09-08 13:06:22 -0400

    formatting and comments

--- bnxc.h
+++ bnxc.h
@@ -21,6 +21,8 @@
 #define LINEMAX 1024
 #define STRN_SIZE 256
 
+/* data structures for database here. */
+
 typedef struct songlist {
 	char name[STRN_SIZE];
 	char path[LINEMAX];
@@ -45,6 +47,7 @@ typedef struct nlist {
 	char name[STRN_SIZE];
 } *list;
 
+/*data structure for ncurses menu. */
 #ifdef FRONTEND_NCURSES
 typedef struct menu_parameters_str {
 	char** list;
@@ -66,34 +69,32 @@ typedef struct menu_parameters_str {
 #endif
 
 /* FOUND IN utils.c */
-char *strip_nl();
-int stringcmp(const void *a, const void *b);
-void destroy_menu_params(menu_parameters params);
+	char *strip_nl();
+	int stringcmp(const void *a, const void *b);
+	void destroy_menu_params(menu_parameters params);
 
 /* FOUND IN db.c */
-unsigned int num_artists;
-int read_db(char *filename);
-void dump_db();
+	unsigned int num_artists;  /*count of how many artists are in the database */
+	int read_db(char *filename);  /*read in the database from a file */
+	void dump_db();  /* dump the database to a file */
 
-list lookup(char *s);
+	list lookup(char *s);  /* look up key (string) in hashtable */
 
-artist add_artist(char *artist_name);
-album add_album(artist art, char *album_name);
-void add_song(album alb, char *trackname, char *path);
+	artist add_artist(char *artist_name);  /* add artist to db */
+	album add_album(artist art, char *album_name);  /* add album to artist in db */
+	void add_song(album alb, char *trackname, char *path);  /* add track to album in db */
 
-char** get_artist_list();
-char** get_album_list(artist art);
-char** get_track_list(album alb);
+	char** get_artist_list();  /* get a list (array of strings) of all the artists) */
+	char** get_album_list(artist art);  /* get a list (array of strings) of all the albums of an artist */
+	char** get_track_list(album alb);  /* get a list (array of strings) of all the tracks in an album */
 
-
+/* FOUND IN frontend.c */
 #ifdef FRONTEND_NCURSES
-	void ninterface();
-	int nmenu(menu_parameters params);
-#endif
-#ifndef FRONTEND_NCURSES
-
+	void ninterface();  /* menu driver */
+	int nmenu(menu_parameters params);  /* menu display and input system */
 #endif
 
+/* FOUND IN player.c */
 #ifdef XMMS2
 	xmmsc_connection_t *connection;
 	xmmsc_result_t *result;
